27847ba6a892af9c0db166530c1cb04eafde1241
RidaLr <rida.laksir@usmba.ac.ma>
2021-06-04 21:54:28 +0200

    Add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
